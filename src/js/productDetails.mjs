e222e8a613d87d0216498e26fa99ba01a61b2259
mjsherwood <mjsherwood@gmail.com>
2023-09-30 13:24:18 -0700

    Add discount to product detail pages

--- src/js/productDetails.mjs
+++ src/js/productDetails.mjs
@@ -6,10 +6,12 @@ export default async function productDetails(productId, selector) {
   const product = await findProductById(productId);
   renderProductDetails(product);
 }
+// render product details
 function renderProductDetails(product) {
   const name = qs("#productName");
   const productNameWithoutBrand = qs("#productNameWithoutBrand");
   const productImage = qs("#productImage");
+  const productSuggestedRetailPrice = qs("#productSuggestedRetailPrice");
   const productFinalPrice = qs("#productFinalPrice");
   const productColorName = qs("#productColorName");
   const productDescriptionHtmlSimple = qs("#productDescriptionHtmlSimple");
@@ -18,11 +20,30 @@ function renderProductDetails(product) {
   name.textContent = product.Name;
   productNameWithoutBrand.textContent = product.NameWithoutBrand;
   productImage.src = product.Image;
-  productFinalPrice.textContent = product.FinalPrice;
+
+  // Format prices to standard dollar format
+  let formattedSuggestedRetailPrice = `$${parseFloat(product.SuggestedRetailPrice).toFixed(2)}`;
+  let formattedFinalPrice = `$${parseFloat(product.FinalPrice).toFixed(2)}`;
+
+  let priceHTML = '';
+  let discountHTML = '';
+
+  if (parseFloat(product.SuggestedRetailPrice) > parseFloat(product.FinalPrice)) {
+    let discountPercent = (((parseFloat(product.SuggestedRetailPrice) - parseFloat(product.FinalPrice)) / parseFloat(product.SuggestedRetailPrice)) * 100).toFixed(0);
+    priceHTML = `<s>${formattedSuggestedRetailPrice}</s> <b>${formattedFinalPrice}</b>`;
+    discountHTML = `<span class="discount">${discountPercent}% off</span>`;
+  } else {
+    priceHTML = formattedFinalPrice;
+  }
+
+  productSuggestedRetailPrice.innerHTML = priceHTML;
+  productFinalPrice.innerHTML = discountHTML;
+
   productColorName.textContent = product.Colors[0].ColorName;
   productDescriptionHtmlSimple.innerHTML = product.DescriptionHtmlSimple;
   btn.dataset.id = product.Id;
 }
+// add to cart
 function addToCart(id, product) {
   setLocalStorage(id, product);
 }
