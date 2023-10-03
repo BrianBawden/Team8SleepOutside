2e45ece54a20b6cf8477482456c276fa5af5accc
Beth Yanez <bethyz@byui.edu>
2023-10-02 18:26:59 -0700

    Added animation to cart icon

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,13 @@
-const cartItems = Object.keys(localStorage);
-const cartItemCount = cartItems.length;
-const cartItemCountElement = document.getElementById("cartItemCount");
-if (cartItemCountElement != null) {
-  cartItemCountElement.textContent = cartItemCount;
+// update cart item count in header
+function updateCartItemCount() {
+  const cartItems = Object.keys(localStorage);
+  const cartItemCount = cartItems.length;
+  const cartItemCountElement = document.getElementById("cartItemCount");
+  if (cartItemCountElement != null) {
+    cartItemCountElement.textContent = cartItemCount;
+    //console.log("cart element exists");
+  }
+  //console.log(cartItemCount);
+  //console.log(cartItemCountElement)
 }
+updateCartItemCount();
