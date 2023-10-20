5d6db033d6c6b7f298fff06b064d96f02db3b335
mjsherwood <mjsherwood@gmail.com>
2023-10-20 16:05:07 -0700

    add alerts bar on home page

--- src/js/main.js
+++ src/js/main.js
@@ -3,10 +3,6 @@ import {
     loadHeaderFooter, 
   } from "./utils.mjs";
 
-import { fetchAlerts } from './utils.mjs';
-
-document.addEventListener('DOMContentLoaded', fetchAlerts);
-
 // load header and footer
 loadHeaderFooter();
 
